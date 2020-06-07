9f2b8e71e6191268933dcd1d9e9bb7388e7b091c
Filipx21 <filip.zajac2277@gmail.com>
2020-06-07 15:30:11 +0200

    model

--- src/main/java/observer/order/Order.java
+++ src/main/java/observer/order/Order.java
@@ -5,4 +5,25 @@ public class Order {
     private Long orderNr;
     private OrderStatus orderStatus;
 
+    public Order(Long orderNr, OrderStatus orderStatus) {
+        this.orderNr = orderNr;
+        this.orderStatus = orderStatus;
+    }
+
+    public Long getOrderNr() {
+        return orderNr;
+    }
+
+    public void setOrderNr(Long orderNr) {
+        this.orderNr = orderNr;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
 }
